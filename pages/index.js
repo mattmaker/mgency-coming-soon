977d3fe9529d1479b94c47d5fb3f65bc90086bdb
Matt Safaii <matt@mgency.com>
2021-12-31 14:43:27 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>MGENCY - A Webflow & No-Code Agency</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="We are currently re-building our website in Webflow!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          MGENCY is a no-code agency specialising in Brand Strategy, Webflow sites, and SEO! We work with B2C tech startups, local businesses, food & beverage, and e-commerce.
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }

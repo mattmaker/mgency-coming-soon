a899ddac7211ff5bfdc7e83656cdacb83e112594
Matt Safaii <matt@mgency.com>
2021-12-31 14:49:38 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,22 @@ export default function Home() {
       <Head>
         <title>MGENCY - A Webflow & No-Code Agency</title>
         <link rel="icon" href="/favicon.ico" />
+        <style>
+          background-color: #131313;
+          color: #ffffff;
+        </style>
       </Head>
 
       <main>
         <Header title="We are currently re-building our website in Webflow!" />
         <p className="description">
-          MGENCY is a no-code agency specialising in Brand Strategy, Webflow sites, and SEO! We work with B2C tech startups, local businesses, food & beverage, and e-commerce.
+          MGENCY is a no-code agency specialising in Brand Strategy, Webflow sites, and SEO!
+        </p>
+        <p className="description>
+          We work with B2C tech startups, local businesses, food & beverage, and e-commerce.
+        </p>
+        <p className="description>
+          For inquiries, please contact info@mgency.com
         </p>
       </main>
     </div>
